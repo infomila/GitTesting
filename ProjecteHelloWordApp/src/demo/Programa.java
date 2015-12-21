a0a7b66babe4197df160b616fee850b103d4a15d
Bernat <bernat.orellana@gmail.com>
2015-12-21 13:17:50 +0100

    Commit inicial

---
+++ ProjecteHelloWordApp/src/demo/Programa.java
@@ -0,0 +1,16 @@
+package demo;
+
+/**
+ *
+ * @author bernat
+ */
+public class Programa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
