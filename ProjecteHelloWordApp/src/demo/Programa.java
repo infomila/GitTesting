08917f321445f7acfb339f074eee94ad27c8baa1
Bernat <bernat.orellana@gmail.com>
2016-01-11 12:10:15 +0100

    Primer commit de la versió 2

--- ProjecteHelloWordApp/src/demo/Programa.java
+++ ProjecteHelloWordApp/src/demo/Programa.java
@@ -11,6 +11,10 @@ public class Programa {
      */
     public static void main(String[] args) {
         System.out.println("Hola Món !");
+        System.out.println("Hola Món !");
+        System.out.println("Hola Món !");
+        System.out.println("Hola Món !");
+        System.out.println("Hola Món !");
     }
     
 }
