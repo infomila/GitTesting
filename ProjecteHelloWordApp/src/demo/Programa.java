431afc89844a94915c8cfd65381346b3ced91987
Paco <bernat.orellana@gmail.com>
2016-01-21 13:28:01 +0100

    Commit den Bernat a la master

--- ProjecteHelloWordApp/src/demo/Programa.java
+++ ProjecteHelloWordApp/src/demo/Programa.java
@@ -10,6 +10,9 @@ public class Programa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+		
+		System.out.println("Canvi master Bernat !");
+		
         System.out.println("Hola Món !");
         System.out.println("Hola Món !");
         System.out.println("Hola Món !");
